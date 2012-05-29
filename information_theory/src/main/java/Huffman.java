a14db94b7b2e3f0b8e14400e3b5e65e049f7d246
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-05-30 02:49:04 +0300

    Make generic Huffman. Fix bug in encodeNumber function.

--- information_theory/src/main/java/Huffman.java
+++ information_theory/src/main/java/Huffman.java
@@ -1,49 +1,23 @@
+import org.apache.commons.io.IOUtils;
+
 import java.io.*;
 import java.util.*;
 
 public class Huffman {
 
     private String getText() throws Exception {
-        StringBuilder builder = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
-        while (reader.ready()) {
-            builder.append(reader.readLine());
-        }
-        reader.close();
-        return builder.toString();
+        return IOUtils.readLines(new FileReader("text.txt")).get(0);
     }
 
-    private Map<Character, Double> getFrequences(String text) {
-        Map<Character, Double> frequences = new HashMap<Character, Double>();
-        for (char ch : text.toCharArray()) {
-            if (!frequences.containsKey(ch)) {
-                frequences.put(ch, 0.0);
-            }
-            frequences.put(ch, frequences.get(ch) + 1);
-        }
-        for (char ch : frequences.keySet()) {
-            frequences.put(ch, frequences.get(ch) / text.length());
-        }
-        return frequences;
-    }
-
-    private double calcEntropy(Map<Character, Double> frequences) {
-        double result = 0;
-        for (double prob : frequences.values()) {
-            result += prob * Math.log(prob) / Math.log(2);
-        }
-        return -result;
-    }
-
-    private Map<Character, String> huffman(Map<Character, Double> frequences) {
-        List<List<Character>> groups = new ArrayList<List<Character>>();
+    private <T> Map<T, String> huffman(Map<T, Double> frequences) {
+        List<List<T>> groups = new ArrayList<List<T>>();
         List<Double> probs = new ArrayList<Double>();
-        Map<Character, String> codes = new HashMap<Character, String>();
-        for (char ch : frequences.keySet()) {
-            codes.put(ch, "");
-            probs.add(frequences.get(ch));
-            List<Character> group = new ArrayList<Character>();
-            group.add(ch);
+        Map<T, String> codes = new HashMap<T, String>();
+        for (T obj : frequences.keySet()) {
+            codes.put(obj, "");
+            probs.add(frequences.get(obj));
+            List<T> group = new ArrayList<T>();
+            group.add(obj);
             groups.add(group);
         }
         while (groups.size() > 1) {
@@ -61,12 +35,12 @@ public class Huffman {
                 mn = mn2;
                 mn2 = tmp;
             }
-            List<Character> group = groups.remove(mn2);
-            for (char ch : group) {
-                codes.put(ch, "1" + codes.get(ch));
+            List<T> group = groups.remove(mn2);
+            for (T obj : group) {
+                codes.put(obj, "1" + codes.get(obj));
             }
-            for (char ch : groups.get(mn)) {
-                codes.put(ch, "0" + codes.get(ch));
+            for (T obj : groups.get(mn)) {
+                codes.put(obj, "0" + codes.get(obj));
             }
             group.addAll(groups.remove(mn));
             groups.add(group);
@@ -76,25 +50,95 @@ public class Huffman {
         return codes;
     }
 
-    private double averageLength(Map<Character, Double> frequences, Map<Character, String> codes) {
-        double result = 0;
-        for (char ch : codes.keySet()) {
-            result += codes.get(ch).length() * frequences.get(ch);
+    private <T> List<String> encode(Map<T, String> codes, Collection<T> collection) {
+        List<String> result = new ArrayList<String>(collection.size());
+        for (T obj : collection) {
+            result.add(codes.get(obj));
         }
         return result;
     }
 
+    private <T> double averageLength(Map<T, Double> frequences, Map<T, String> codes) {
+        double result = 0;
+        for (T obj : codes.keySet()) {
+            result += codes.get(obj).length() * frequences.get(obj);
+        }
+        return result;
+    }
+
+    private <T> Map<T, String> runFrequencies(Map<T, Double> frequencies) throws Exception {
+        l(frequencies);
+        Map<T, String> codes = huffman(frequencies);
+        l(codes);
+        l("Averate length: " + averageLength(frequencies, codes));
+        l("Entropy: " + Utils.calcEntropy(frequencies));
+        return codes;
+    }
+
+    private <T> void encodeAndLog(Map<T, String> codes, Collection<T> collection) {
+        List<String> encoded = encode(codes, collection);
+        l("Total length " + Utils.totalLength(encoded));
+    }
+
     private void solve() throws Exception {
-        String text = getText();
-        Map<Character, Double> frequences = getFrequences(text);
-        System.out.println(frequences);
-        Map<Character, String> codes = huffman(frequences);
-        System.out.println(codes);
-        System.out.println("Averate length: " + averageLength(frequences, codes));
-        System.out.println("Entropy: " + calcEntropy(frequences));
+        l("Standard frequencies");
+        Map<Character, String> codes = runFrequencies(getRussianAlphabetFrequences());
+        encodeAndLog(codes, Utils.getCharacters(getText()));
+
+        l("");
+        l("Based on text");
+        codes = runFrequencies(Utils.getFrequences(Utils.getCharacters(getText())));
+        encodeAndLog(codes, Utils.getCharacters(getText()));
+
+        l("");
+        l("Double letters");
+        List<String> parts = Utils.split(getText(), 2);
+        Map<String, String> codes2 = runFrequencies(Utils.getFrequences(parts));
+        encodeAndLog(codes2, parts);
     }
 
     public static void main(String[] args) throws Exception {
         new Huffman().solve();
+    }
+
+    private static void l(Object obj) {
+       System.out.println(obj);
+    }
+
+    private static Map<Character, Double> getRussianAlphabetFrequences() {
+        return new HashMap<Character, Double>() {{
+            put(' ', 0.175);
+            put('о', 0.090);
+            put('е', 0.072);
+            put('а', 0.062);
+            put('и', 0.062);
+            put('т', 0.053);
+            put('н', 0.053);
+            put('с', 0.045);
+            put('р', 0.040);
+            put('в', 0.038);
+            put('л', 0.035);
+            put('к', 0.028);
+            put('м', 0.026);
+            put('д', 0.025);
+            put('п', 0.023);
+            put('у', 0.021);
+            put('я', 0.018);
+            put('ы', 0.016);
+            put('з', 0.016);
+            put('ь', 0.014);
+            put('б', 0.014);
+            put('г', 0.013);
+            put('ч', 0.012);
+            put('й', 0.010);
+            put('х', 0.009);
+            put('ж', 0.007);
+            put('ю', 0.006);
+            put('ш', 0.006);
+            put('ц', 0.004);
+            put('щ', 0.003);
+            put('э', 0.003);
+            put('ф', 0.002);
+        }};
     }
 }
--- information_theory/src/main/java/Utils.java
+++ information_theory/src/main/java/Utils.java
@@ -1,7 +1,10 @@
 import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class Utils {
 
@@ -10,7 +13,8 @@ public final class Utils {
 
     public static String encodeNumber(int number) {
         String binary = Integer.toBinaryString(number);
-        String bPref = StringUtils.repeat("0", binary.length() / 2) + Integer.toBinaryString(binary.length());
+        String lenBinary = Integer.toBinaryString(binary.length());
+        String bPref = StringUtils.repeat("0", lenBinary.length() - 1) + lenBinary;
         String bElias = bPref + binary.substring(1);
         return bElias;
     }
@@ -49,5 +53,35 @@ public final class Utils {
             sum += element.length();
         }
         return sum;
+    }
+
+    public static <T> Map<T, Double> getFrequences(Collection<T> collection) {
+        Map<T, Double> frequences = new HashMap<T, Double>();
+        for (T obj : collection) {
+            if (!frequences.containsKey(obj)) {
+                frequences.put(obj, 0.0);
+            }
+            frequences.put(obj, frequences.get(obj) + 1);
+        }
+        for (T obj : frequences.keySet()) {
+            frequences.put(obj, frequences.get(obj) / collection.size());
+        }
+        return frequences;
+    }
+
+    public static <T> double calcEntropy(Map<T, Double> frequences) {
+        double result = 0;
+        for (double prob : frequences.values()) {
+            result += prob * Math.log(prob) / Math.log(2);
+        }
+        return -result;
+    }
+
+    public static List<Character> getCharacters(String str) {
+        List<Character> characters = new ArrayList<Character>(str.length());
+        for (char ch : str.toCharArray()) {
+            characters.add(ch);
+        }
+        return characters;
     }
 }
